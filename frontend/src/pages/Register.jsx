c7d4e7c58f7f609973067074328e1016b7d5d7bb
Vedika0431 <vangarvedika@gmail.com>
2025-05-18 13:01:59 +0530

    added styling to login and register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "../register.css";
 
 function Register() {
   return (
-    <div className="page">
-      <h1>Register</h1>
-      <form>
+    <div className="register-container">
+      <h1 className="register-title">Create an account</h1>
+      <form className="register-form">
+        <div>
+          <label htmlFor="register-firstname">First Name:</label>
+          <input type="text" id="register-firstname" name="firstname" required />
+        </div>
+        <div>
+          <label htmlFor="register-lastname">Last Name:</label>
+          <input type="text" id="register-lastname" name="lastname" required />
+        </div>
         <div>
           <label htmlFor="register-email">Email:</label>
           <input type="email" id="register-email" name="email" required />
@@ -13,8 +22,14 @@ function Register() {
           <label htmlFor="register-password">Password:</label>
           <input type="password" id="register-password" name="password" required />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Create an account</button>
       </form>
+      <p className="register-link-text">
+        Already have an account?{" "}
+        <a href="/login" id="login-link">
+          Login
+        </a>
+      </p>
     </div>
   );
 }
