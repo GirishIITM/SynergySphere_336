701a536242597cd1a12dfb736c4dd5a5f87ee4e3
Vedika0431 <vangarvedika@gmail.com>
2025-05-18 11:12:00 +0530

    login and register frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-import "../styles/login.css";
-export default function Login() {
+import React from "react";
+
+function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="page">
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="login-email">Email:</label>
+          <input type="email" id="login-email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="login-password">Password:</label>
+          <input type="password" id="login-password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
+
+export default Login;
